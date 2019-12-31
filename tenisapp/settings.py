1e103f5dd11370099bc20c0b7a765d0e81dbc070
dipzza <dipzza@protonmail.ch>
2019-12-31 01:28:13 +0100

    Fixed route for secrets_settings import

--- tenisapp/settings.py
+++ tenisapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import secret_settings
+import tenisapp.secret_settings as secret_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
